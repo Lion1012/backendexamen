b65c0906a473540a58cba793d8d3820b9f23f4ed
Lion1012 <leoncebem@gmail.com>
2022-06-22 12:28:54 +0200

    apiarreglada

--- src/main/java/com/sistemas/informaticos/controllers/Main.java
+++ src/main/java/com/sistemas/informaticos/controllers/Main.java
@@ -1,6 +1,7 @@
 package com.sistemas.informaticos.controllers;
 
 import com.sistemas.informaticos.models.Results;
+import com.sistemas.informaticos.services.ElevationService;
 import com.sistemas.informaticos.services.PopulationService;
 import com.sistemas.informaticos.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class Main {
     
     @Autowired
-    PopulationService elevationService;
+    ElevationService elevationService;
 
 
     @GetMapping("/")
     public String inicio() {
         return "Mi servidor backend examen";
     }
-    
+    /*
+     * 
     @GetMapping("/coincidencias")
     public String concideLetras(@RequestParam String nombre1, @RequestParam String nombre2) {
         return nombre1 + nombre2 + " tienen " + Utils.calculaCoincidencias(nombre1, nombre2) + " letras en comun";
     }
-    
+   */ 
+  @GetMapping("/contar/{palabra}")
+    public String cuentaletras(@PathVariable String palabra) {
+        return palabra + " tiene " + Utils.cuentaLetraA(palabra);
+    }
+
     @GetMapping("/elevacion/{lugar}")
-    public String getElevation(@PathVariable String elevacion){
-        Results results = elevationService.getPopulation(elevacion);
+    public String getElevation(@PathVariable String lugar){
+        Results results = elevationService.getElevation(lugar);
         return results.results[0].elevation + " m";
     }
 }
